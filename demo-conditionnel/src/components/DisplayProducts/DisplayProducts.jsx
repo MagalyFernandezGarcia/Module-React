a210f1d11d773d701f639ef60464abf7a8ce695e
Magaly Fernandez Garcia <fernandezgarciamagaly@gmail.com>
2024-09-17 13:26:57 +0200

    correcting bug

--- demo-conditionnel/src/components/DisplayProducts/DisplayProducts.jsx
+++ demo-conditionnel/src/components/DisplayProducts/DisplayProducts.jsx
@@ -1,16 +1,26 @@
 import style from "./DisplayProducts.module.css";
 
-const ProductPresent = ({ name, description = null, price, promo = null }) => {
+const ProductPresent = ({ name, description, price, promotion }) => {
 	const priceArrange = price.toLocaleString("fr-FR", {
 		style: "currency",
 		currency: "EUR",
 	});
+
+	const imgUrl = new URL(`./img/${name}.jpg`, import.meta.url);
 	return (
 		<section className={style["card"]}>
 			<p>{name}</p>
+			{/* <img src={"./img/" + name + ".jpg"} alt="" /> */}
+			<img className={style["img"]} src={imgUrl.href} alt="" />
+
 			<p>{description}</p>
-			<p className={promo ? "isPromo" : null}>{priceArrange}</p>
-			{promo ? <p className={style["promo"]}>promo</p> : null}
+			{promotion ? (
+				<p className={style["isPromo"]}>{priceArrange}</p>
+			) : (
+				<p>{priceArrange}</p>
+			)}
+
+			{promotion ? <p className={style["promo"]}>promo</p> : null}
 		</section>
 	);
 };
