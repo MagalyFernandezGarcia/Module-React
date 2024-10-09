8eb888a772b2c3cd7c85a1f2a4af429c433bfe0b
Magaly Fernandez Garcia <fernandezgarciamagaly@gmail.com>
2024-10-09 16:28:19 +0200

    resolving bug

--- demo-react-redux/src/containers/Country/CountryRequest.tsx
+++ demo-react-redux/src/containers/Country/CountryRequest.tsx
@@ -2,17 +2,14 @@ import { useDispatch } from "react-redux";
 import { countrySearch } from "../../store/countries/countries.action";
 import SearchBarrCorr from "../../components/SearchBarCorr/SearchBarrCorr";
 
-
-
 const CountryRequest = () => {
-    const dispatch = useDispatch<any>()
-    const handleSearch =(name :string){
-        dispatch(countrySearch(name))
-        
-    }
+	const dispatch = useDispatch<any>();
+	const handleSearch = (name: string) => {
+		dispatch(countrySearch(name));
+	};
 	return (
 		<>
-			<SearchBarrCorr onSearchCorr={handleSearch}/>
+			<SearchBarrCorr onSearchCorr={handleSearch} />
 		</>
 	);
 };
